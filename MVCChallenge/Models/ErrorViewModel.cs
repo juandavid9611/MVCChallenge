6aff69a02c19eb3132d4e84ee33e37ec1fd7da99
jarodriguez <jd_rodrigueza@javeriana.edu.co>
2019-02-26 14:45:54 -0500

    Add project files.

---
+++ MVCChallenge/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCChallenge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
